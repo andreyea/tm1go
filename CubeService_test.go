030fa18f79ff3829024fc028db28176a5accd5d9
andreyea <andy86e@gmail.com>
2024-07-07 17:06:33 +0900

    New test cases for cube service

--- CubeService_test.go
+++ CubeService_test.go
@@ -211,3 +211,106 @@ func TestCubeService_GetMeasureDimension(t *testing.T) {
 		})
 	}
 }
+
+func TestCubeService_Update(t *testing.T) {
+	// Create dimensions
+	err := tm1ServiceT.DimensionService.Create(&tm1go.Dimension{Name: "TestD1"})
+	if err != nil {
+		t.Errorf("Error creating dimension: %v", err)
+	}
+	err = tm1ServiceT.DimensionService.Create(&tm1go.Dimension{Name: "TestD2"})
+	if err != nil {
+		t.Errorf("Error creating dimension: %v", err)
+	}
+
+	// Create a cube
+	err = tm1ServiceT.CubeService.Create(tm1go.Cube{Name: "TestCube1", Dimensions: []tm1go.Dimension{{Name: "TestD1"}, {Name: "TestD2"}}, Rules: ""})
+	if err != nil {
+		t.Errorf("Error creating cube: %v", err)
+	}
+
+	tests := []struct {
+		name    string
+		cube    tm1go.Cube
+		wantErr bool
+	}{
+		{
+			name:    "Valid cube - no rules",
+			cube:    tm1go.Cube{Name: "TestCube1", Dimensions: []tm1go.Dimension{{Name: "TestD1"}, {Name: "TestD2"}}, Rules: ""},
+			wantErr: false,
+		},
+		{
+			name:    "Valid cube - with rules",
+			cube:    tm1go.Cube{Name: "TestCube1", Dimensions: []tm1go.Dimension{{Name: "TestD1"}, {Name: "TestD2"}}, Rules: "#this is a rule"},
+			wantErr: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := tm1ServiceT.CubeService.Update(tt.cube)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("CubeService.Update() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+
+	t.Cleanup(func() {
+		// Delete cubes and dimensions
+		tm1ServiceT.CubeService.Delete("TestCube1")
+		tm1ServiceT.DimensionService.Delete("TestD1")
+		tm1ServiceT.DimensionService.Delete("TestD2")
+	})
+}
+
+func TestCubeService_UpdateOrCreate(t *testing.T) {
+	// Create dimensions
+	err := tm1ServiceT.DimensionService.Create(&tm1go.Dimension{Name: "TestD1"})
+	if err != nil {
+		t.Errorf("Error creating dimension: %v", err)
+	}
+	err = tm1ServiceT.DimensionService.Create(&tm1go.Dimension{Name: "TestD2"})
+	if err != nil {
+		t.Errorf("Error creating dimension: %v", err)
+	}
+
+	// Create a cube
+	err = tm1ServiceT.CubeService.Create(tm1go.Cube{Name: "TestCube1", Dimensions: []tm1go.Dimension{{Name: "TestD1"}, {Name: "TestD2"}}, Rules: ""})
+	if err != nil {
+		t.Errorf("Error creating cube: %v", err)
+	}
+
+	tests := []struct {
+		name    string
+		cube    tm1go.Cube
+		wantErr bool
+	}{
+		{
+			name:    "Valid cube - with rules",
+			cube:    tm1go.Cube{Name: "TestCube1", Dimensions: []tm1go.Dimension{{Name: "TestD1"}, {Name: "TestD2"}}, Rules: "#this is a rule"},
+			wantErr: false,
+		},
+		{
+			name:    "Valid cube - non existent cube",
+			cube:    tm1go.Cube{Name: "TestCube2", Dimensions: []tm1go.Dimension{{Name: "TestD1"}, {Name: "TestD2"}}, Rules: "#this is a rule"},
+			wantErr: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := tm1ServiceT.CubeService.UpdateOrCreate(tt.cube)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("CubeService.UpdateOrCreate() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+
+	t.Cleanup(func() {
+		// Delete cubes and dimensions
+		tm1ServiceT.CubeService.Delete("TestCube1")
+		tm1ServiceT.CubeService.Delete("TestCube2")
+		tm1ServiceT.DimensionService.Delete("TestD1")
+		tm1ServiceT.DimensionService.Delete("TestD2")
+	})
+}
