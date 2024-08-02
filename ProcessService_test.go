774cf653266cf39b72c81e05cf47cbeba6eeb0fe
andreyea <andy86e@gmail.com>
2024-08-02 17:22:01 +1000

    Test GetAll func

--- ProcessService_test.go
+++ ProcessService_test.go
@@ -30,3 +30,38 @@ func TestProcessService_Get(t *testing.T) {
 	}
 
 }
+
+func TestProcessService_GetAll(t *testing.T) {
+	type args struct {
+		skipControlProcesses bool
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name:    "Get all processes",
+			args:    args{skipControlProcesses: false},
+			wantErr: false,
+		},
+		{
+			name:    "Get all processes except control processes",
+			args:    args{skipControlProcesses: true},
+			wantErr: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			process, err := tm1ServiceT.ProcessService.GetAll(tt.args.skipControlProcesses)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("TestProcessService.GetAll() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if !tt.wantErr && process == nil {
+				t.Errorf("TestProcessService.GetAll() error = %v, wantErr %v", "No processes were returned", tt.wantErr)
+			}
+		})
+	}
+
+}
